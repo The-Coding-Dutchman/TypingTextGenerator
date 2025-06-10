a1b139808d41f0b0243f701e993478488f5dff25
Coding Dutchman <stuupiid0@gmail.com>
2025-06-10 21:38:27 +0200

    addedoptions

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ public class Main {
 
 	public static void main(String... args){
 
-	
+		StringBuilder charsToLearn;
+		StringBuilder alphabet = new StringBuilder("aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ");
+		StringBuilder punctuation = new StringBuilder(".,");
+		StringBuilder symbols = new StringBuilder("=@$%!+*&<>~_-:;|\"\'\\\/");
+		StringBuilder numbers = new StringBuilder("0123456789");
+
+		
 
 
 
