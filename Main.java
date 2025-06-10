a64a2d7e3dec442271673c960ee03f0f9711589b
Coding Dutchman <stuupiid0@gmail.com>
2025-06-10 21:52:35 +0200

    added_user_input_no_functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-package main
+package Main;
 
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
 
@@ -8,10 +10,21 @@ public class Main {
 		StringBuilder charsToLearn;
 		StringBuilder alphabet = new StringBuilder("aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ");
 		StringBuilder punctuation = new StringBuilder(".,");
-		StringBuilder symbols = new StringBuilder("=@$%!+*&<>~_-:;|\"\'\\\/");
+		StringBuilder symbols = new StringBuilder("=@$%!+*&<>~_-:;|\"\'\\/");
 		StringBuilder numbers = new StringBuilder("0123456789");
 
-		
+		HashMap<String, String> userSelection = new HashMap<>();
+
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.println("");
+		System.out.println("Welcome to the text generator. Please select all the componennts you want to test. They will be listed one by one");
+		System.out.println("Continue?");
+		System.out.println("Y/N");
+		if(userInput.nextLine().toUpperCase() == "Y") {
+
+
+		}
 
 
 
